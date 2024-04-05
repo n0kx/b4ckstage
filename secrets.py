0ce81a0ff69698a30f07696854cbe1c194f70459
n0kx <tgregoryknox@gmail.com>
2024-04-04 22:47:47 -0400

    Adding secrets.py
    Adding secrets.py

--- secrets.py
+++ secrets.py
@@ -1,3 +1,3 @@
-WIFI_SSID = "WingWong"
-WIFI_PASSWORD = "getmeonline"
-PRODUCTION_URL = "https://n0kx.com/b4ckstage/api/"
+WIFI_SSID = "SSID"
+WIFI_PASSWORD = "PASSWORD"
+PRODUCTION_URL = "https://n0kx.com/b4ckstage/"
